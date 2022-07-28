387612b58ccd30eec647a21253e04f542677c30f
unknown <yMarechal@gmail.com>
2022-07-28 12:18:23 +0200

    creation table reunion

---
+++ src/Entity/Reunion.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReunionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReunionRepository::class)]
+class Reunion
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $objet;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $lieu;
+
+    #[ORM\Column(type: 'date')]
+    private $date;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $participants;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $contenu;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getObjet(): ?string
+    {
+        return $this->objet;
+    }
+
+    public function setObjet(string $objet): self
+    {
+        $this->objet = $objet;
+
+        return $this;
+    }
+
+    public function getLieu(): ?string
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(string $lieu): self
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getParticipants(): ?string
+    {
+        return $this->participants;
+    }
+
+    public function setParticipants(string $participants): self
+    {
+        $this->participants = $participants;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+}
